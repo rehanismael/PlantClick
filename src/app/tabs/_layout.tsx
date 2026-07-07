104218fdfa4d50587b2ed0fdc3fb22cc35976306
rehanismael <rehanismail.dev@gmail.com>
2026-07-07 21:02:15 +0500

    littlebit changesin ui

--- src/app/tabs/_layout.tsx
+++ src/app/tabs/_layout.tsx
@@ -3,18 +3,53 @@ import { Camera, History, House } from "lucide-react-native";
 
 export default function _layout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          backgroundColor: "#f9fbf4",
+          borderRadius: 28,
+          height: 70,
+          // width: "90%",
+          bottom: 20,
+          marginLeft: 12,
+          marginRight: 12,
+          // shadowColor: "transparent",
+          shadowOpacity: 0,
+        },
+        tabBarItemStyle: {
+          justifyContent: "center", // Centers content vertically
+          alignItems: "center", // Centers content horizontally
+          paddingTop: 5, // Adds a touch of breathing room at the top
+        },
+        tabBarLabelStyle: {
+          fontFamily: "Manrope-Bold",
+          fontSize: 10,
+          color: "#5d8a52",
+        },
+      }}
+    >
       <Tabs.Screen
         name="home"
-        options={{ title: "Home", tabBarIcon: () => <House size={25} /> }}
+        options={{
+          title: "Home",
+          tabBarIcon: () => <House color="#5d8a52" size={20} />,
+        }}
       />
       <Tabs.Screen
         name="camera"
-        options={{ title: "Camera", tabBarIcon: () => <Camera size={25} /> }}
+        options={{
+          title: "Camera",
+          tabBarIcon: () => <Camera color="#5d8a52" size={20} />,
+        }}
       />
       <Tabs.Screen
         name="history"
-        options={{ title: "History", tabBarIcon: () => <History size={25} /> }}
+        options={{
+          title: "History",
+          tabBarIcon: () => <History color="#5d8a52" size={20} />,
+        }}
       />
     </Tabs>
   );
--- src/app/tabs/home.tsx
+++ src/app/tabs/home.tsx
@@ -3,7 +3,12 @@ import { StyleSheet, View } from "react-native";
 const home = () => {
   return (
     <View
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f5f7e8",
+      }}
     ></View>
   );
 };
